8897f5daf794bf8fa202b7533b933cb6c00d80b8
elcordova <erick.lenin.03@gmail.com>
2016-10-19 11:57:30 -0500

    menu_admin

--- application/views/administracion/administra.php
+++ application/views/administracion/administra.php
@@ -3,79 +3,76 @@
 		if ($this->session->userdata('conectado')==true) {
 			$username=$this->session->userdata('email');
 			$id_user=$this->session->userdata('id');
-		?>
+			?>
 			<?php include("includes/cabecera.php") ?>
+			
 			<body id="page-top" data-spy="scroll" data-target=".navbar-custom">
-	<!-- Preloader -->
-			<div id="preloader">
-			  <div id="load"></div>
-			</div>
-			<?php include("includes/navbar.php") ?>
+				<!-- Preloader -->
+				<div id="preloader">
+					<div id="load"></div>
+				</div>
+				<?php include("includes/navbar.php") ?>
 				<div class="container">
-		<div class="row">
-			<div class="col-lg-2 col-lg-offset-5">
-				<hr class="marginbot-50">
-			</div>
-		</div>
-        <div class="row">
-            <div class="col-xs-6 col-sm-3 col-md-3">
-				<div class="wow bounceInUp" data-wow-delay="0.2s">
-                <div class="team boxed-grey">
-                    <div class="inner">
-						<h5>Dra. Maritza Agudo</h5>
-                        <p class="subtitle">Gestora del Proyecto</p>
-                        <div class="avatar"><img src="<?=base_url()?>static/img/team/1.jpg" alt="" class="img-responsive img-circle" /></div>
-                    </div>
-                </div>
-				</div>
-            </div>
-			<div class="col-xs-6 col-sm-3 col-md-3">
-				<div class="wow bounceInUp" data-wow-delay="0.5s">
-                <div class="team boxed-grey">
-                    <div class="inner">
-						<h5>Dr. Nombre 2</h5>
-                        <p class="subtitle">Cargo 2</p>
-                        <div class="avatar"><img src="<?=base_url()?>static/img/team/2.jpg" alt="" class="img-responsive img-circle" /></div>
 
-                    </div>
-                </div>
-				</div>
-            </div>
-			<div class="col-xs-6 col-sm-3 col-md-3">
-				<div class="wow bounceInUp" data-wow-delay="0.8s">
-                <div class="team boxed-grey">
-                    <div class="inner">
-						<h5>Dr. Nombre 3</h5>
-                        <p class="subtitle">Cargo 3</p>
-                        <div class="avatar"><img src="<?=base_url()?>static/img/team/3.jpg" alt="" class="img-responsive img-circle" /></div>
+					<div class="row">
+						<div class="col-xs-6 col-sm-3 col-md-3">
+							<div class="wow bounceInUp" data-wow-delay="0.2s">
+								<div class="team boxed-grey">
+									<div class="inner">
+										<h5>Administra</h5>
+										<p class="subtitle">Usuarios</p>
+										<div class="avatar"><img src="<?=base_url()?>static/images/user_admin.png" alt="" class="img-responsive img-rounded" /></div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-xs-6 col-sm-3 col-md-3">
+							<div class="wow bounceInUp" data-wow-delay="0.5s">
+								<div class="team boxed-grey">
+									<div class="inner">
+										<h5>Administra</h5>
+										<p class="subtitle">Noticias</p>
+										<div class="avatar"><img src="<?=base_url()?>static/images/megafone.png" alt="" class="img-responsive img-rounded" /></div>
 
-                    </div>
-                </div>
-				</div>
-            </div>
-			<div class="col-xs-6 col-sm-3 col-md-3">
-				<div class="wow bounceInUp" data-wow-delay="1s">
-                <div class="team boxed-grey">
-                    <div class="inner">
-						<h5>Dr. Nombre 4</h5>
-                        <p class="subtitle">Cargo 4</p>
-                        <div class="avatar"><img src="<?=base_url()?>static/img/team/4.jpg" alt="" class="img-responsive img-circle" /></div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-xs-6 col-sm-3 col-md-3">
+							<div class="wow bounceInUp" data-wow-delay="0.8s">
+								<div class="team boxed-grey">
+									<div class="inner">
+										<h5>Administra</h5>
+										<p class="subtitle">Eventos</p>
+										<div class="avatar"><img src="<?=base_url()?>static/images/event.png" alt="" class="img-responsive img-rounded" /></div>
 
-                    </div>
-                </div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-xs-6 col-sm-3 col-md-3">
+							<div class="wow bounceInUp" data-wow-delay="1s">
+								<div class="team boxed-grey">
+									<div class="inner">
+										<h5>Administra</h5>
+										<p class="subtitle">Brigadas</p>
+										<div class="avatar"><img src="<?=base_url()?>static/images/brigadas.png" alt="" class="img-responsive img-rounded" /></div>
+
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>		
 				</div>
-            </div>
-        </div>		
-		</div>
 
 				<?php include("includes/footer.php") ?>
-		<?php 
-		} else 
-		{
-			redirect(base_url());
-		}
-		 ?>
+				<?php 
+			} else 
+			{
+				redirect(base_url());
+			}
+			?>
 
-		 </body>
-			</html>
+		</body>
+		</html>
 
--- application/views/administracion/includes/navbar.php
+++ application/views/administracion/includes/navbar.php
@@ -1,10 +1,10 @@
-<nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
+<nav class="navbar navbar-custom" role="navigation">
         <div class="container">
             <div class="navbar-header page-scroll">
                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
                     <i class="fa fa-bars"></i>
                 </button>
-                <a class="navbar-brand" href="index.html">
+                <a class="navbar-brand" href="Administracion">
                     <img src="<?=base_url()?>static/img/logo2.png" alt="" class="img-responsive img-responsive" />
                 </a>
             </div>
@@ -23,25 +23,3 @@
         </div>
         <!-- /.container -->
 </nav>
-
-<div class="modal fade" id="modal1" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content modal-popup">
-                    <a href="#" class="close-link"><i class="close"></i></a>
-                    <h3 class="white">Acceder al Sistema</h3>
-                    <form action="" method="POST" accept-charset="utf-8" class="popup-form">
-                        <input type="text" class="form-control form-white" placeholder="Usuario" id="username" name="username">
-                        <input type="password" class="form-control form-white" placeholder="Contraseña" name="password" id="password">
-                        <input type="hidden" name="list" value="L1JC1AAR7217B8f3PIl07g"/>
-                        
-                        <div class="checkbox-holder text-left">
-                            <div class="checkbox">
-                                <input type="checkbox" value="None" id="squaredOne" name="check" />
-                                <label for="squaredOne"><span>Recordarme.</span></label>
-                            </div>
-                        </div>
-                        <button type="submit" class="btn btn-submit">Ingresar</button>
-                    </form>
-                </div>
-            </div>
-</div>
