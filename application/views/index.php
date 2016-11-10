0fe4b82a3afe4590bcc72a7f33124f66aaafae3e
alexMS007 <alexd_8425@hotmail.com>
2016-11-10 12:38:21 -0500

    redes

--- application/views/index.php
+++ application/views/index.php
@@ -373,12 +373,11 @@
 				<address>
 				  <strong>Estamos en las redes sociales</strong><br>
                        	<ul class="company-social">
-<<<<<<< HEAD
+
                             <li class="social-facebook"><a href="https://www.facebook.com/ceupropsf/" target="_blank"><i class="fa fa-facebook"></i></a></li>
                             <li class="social-twitter"><a href="https://twitter.com/ceupropsf" target="_blank"><i class="fa fa-twitter"></i></a></li>
                             <li class="social-google"><a href="https://plus.google.com/108705010349767747804" target="_blank"><i class="fa fa-google-plus"></i></a></li>
-=======
->>>>>>> origin/contactenos
+
                         </ul>	
 				</address>					
 			
