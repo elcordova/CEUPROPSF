6698e7771bd699b46345bd53edfe4017b5d7d44f
Alejandro <alejomanu21@gmail.com>
2017-01-22 20:32:08 -0500

    se modifico el diseño del administrador

---
+++ application/views/menui.php
@@ -0,0 +1,7 @@
+
+            
+	<div id="page-content-wrapper">
+	        <a href="<?php echo base_url()?>"><img alt="image" src="<?php echo base_url()?>static/img/bg141.jpg" />
+	</div>   
+
+                
